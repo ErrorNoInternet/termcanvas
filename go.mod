a5522db792d7d5ce82ee4b0ee8938d7be4026f9e
ErrorNoInternet <error.nointernet@gmail.com>
2022-07-29 13:55:22 +0800

    Initial commit

---
+++ go.mod
@@ -0,0 +1,17 @@
+module github.com/ErrorNoInternet/termcanvas
+
+go 1.18
+
+require (
+	github.com/gdamore/tcell/v2 v2.5.1
+	github.com/mattn/go-runewidth v0.0.13
+)
+
+require (
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	golang.org/x/sys v0.0.0-20220318055525-2edf467146b5 // indirect
+	golang.org/x/term v0.0.0-20201210144234-2321bbc49cbf // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gdamore/tcell/v2"
+	"github.com/gdamore/tcell/v2/encoding"
+)
+
+var (
+	block  rune     = '█'
+	colors []string = []string{
+		"black",
+		"maroon",
+		"green",
+		"olive",
+		"navy",
+		"purple",
+		"teal",
+		"silver",
+		"grey",
+		"red",
+		"lime",
+		"yellow",
+		"blue",
+		"fuchsia",
+		"aqua",
+		"white",
+	}
+	selectedTool  string
+	selectedColor string
+)
+
+func drawRegion(screen tcell.Screen, x1, y1, x2, y2 int, style tcell.Style, letter rune, drawBorders bool) {
+	if y2 < y1 {
+		y1, y2 = y2, y1
+	}
+	if x2 < x1 {
+		x1, x2 = x2, x1
+	}
+	borderStyle := tcell.StyleDefault.Foreground(tcell.ColorWhite)
+
+	if drawBorders {
+		for col := x1; col <= x2; col++ {
+			screen.SetContent(col, y1, tcell.RuneHLine, nil, borderStyle)
+			screen.SetContent(col, y2, tcell.RuneHLine, nil, borderStyle)
+		}
+		for row := y1 + 1; row < y2; row++ {
+			screen.SetContent(x1, row, tcell.RuneVLine, nil, borderStyle)
+			screen.SetContent(x2, row, tcell.RuneVLine, nil, borderStyle)
+		}
+		if y1 != y2 && x1 != x2 {
+			screen.SetContent(x1, y1, tcell.RuneULCorner, nil, borderStyle)
+			screen.SetContent(x2, y1, tcell.RuneURCorner, nil, borderStyle)
+			screen.SetContent(x1, y2, tcell.RuneLLCorner, nil, borderStyle)
+			screen.SetContent(x2, y2, tcell.RuneLRCorner, nil, borderStyle)
+		}
+	}
+	for row := y1 + 1; row < y2; row++ {
+		for col := x1 + 1; col < x2; col++ {
+			screen.SetContent(col, row, letter, nil, style)
+		}
+	}
+}
+
+func main() {
+	encoding.Register()
+	screen, err := tcell.NewScreen()
+	if err != nil {
+		fmt.Printf("Unable to create screen: %v\n", err.Error())
+		os.Exit(1)
+	}
+	if err := screen.Init(); err != nil {
+		fmt.Printf("Unable to create screen: %v\n", err.Error())
+		os.Exit(1)
+	}
+	background := tcell.StyleDefault.Background(tcell.ColorBlack)
+	defaultStyle := tcell.StyleDefault.
+		Background(tcell.ColorReset).
+		Foreground(tcell.ColorReset)
+	screen.SetStyle(defaultStyle)
+	screen.EnableMouse()
+	screen.EnablePaste()
+	screen.Clear()
+	width, height := screen.Size()
+	colorsOffset := 7
+
+	for {
+		screen.Show()
+		event := screen.PollEvent()
+		width, height = screen.Size()
+
+		drawRegion(screen, colorsOffset-1, 0, len(colors)+colorsOffset, 3, defaultStyle, ' ', true)
+		for index, color := range colors {
+			drawRegion(screen, index+(colorsOffset-1), 0, index+(colorsOffset+1), 3, tcell.StyleDefault.Foreground(tcell.GetColor(color)), block, false)
+		}
+		drawRegion(screen, 0, 0, 5, 3, tcell.StyleDefault.Foreground(tcell.GetColor(selectedColor)), block, true)
+
+		switch event := event.(type) {
+		case *tcell.EventKey:
+			if event.Key() == tcell.KeyEscape {
+				screen.Fini()
+				os.Exit(0)
+			}
+		case *tcell.EventResize:
+			screen.Sync()
+		case *tcell.EventMouse:
+			x, y := event.Position()
+			button := event.Buttons()
+			if button == 1 {
+				if y == 1 || y == 2 {
+					if x-colorsOffset < len(colors) && x-colorsOffset >= 0 {
+						selectedColor = colors[x-colorsOffset]
+					}
+				}
+				screen.SetContent(x, y, block, nil, tcell.StyleDefault.Foreground(tcell.GetColor(selectedColor)))
+			} else if button == 2 {
+				screen.SetContent(x, y, ' ', nil, defaultStyle)
+			}
+		default:
+			screen.SetContent(width-1, height-1, 'X', nil, background)
+		}
+	}
+}
